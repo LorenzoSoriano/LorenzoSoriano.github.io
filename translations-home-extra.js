db099020d26690dc62cc63278633d546d847890a
LorenzoSoriano <61503460+LorenzoSoriano@users.noreply.github.com>
2026-09-16 00:21:55 +0200

    Add bilingual Remember to Wait home metadata

---
+++ translations-home-extra.js
@@ -0,0 +1,7 @@
+window.TRANSLATIONS = window.TRANSLATIONS || { en:{}, it:{} };
+Object.assign(window.TRANSLATIONS.en, {
+  "home.waitType":"GAME JAM · PROGRAMMING · LEVEL DESIGN"
+});
+Object.assign(window.TRANSLATIONS.it, {
+  "home.waitType":"GAME JAM · PROGRAMMAZIONE · LEVEL DESIGN"
+});
